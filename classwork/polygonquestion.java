ccd6863fb368469f88f3b060d7353bcef5d7a0a3
Daniel DeCarlo <44644735+danielcd987@users.noreply.github.com>
2020-02-10 21:17:22 -0500

    Add files via upload

---
+++ classwork/polygonquestion.java
@@ -0,0 +1,41 @@
+package classwork;
+public class polygonquestion {
+	public static void main(String[] args) {
+		System.out.println(area(0, 0)); //this takes the values of width and length and finds area
+		System.out.println(polygon(4,4));
+		octogon one = 
+	}
+	
+	
+	
+	
+	static int polygon(int base, int height) {
+		int areaTriangle = (int) ((.5)* base * height);
+		return areaTriangle;
+		
+	}
+	
+	
+	
+	static int area(int w, int l){
+	   int areasol = w * l;
+		return areasol;	
+	}
+}
+
+class triangle {
+	
+}
+class quadrilateral{
+	
+}
+class pentegon{
+	
+}
+class hexagon{
+	
+}
+class octogon {
+	private int side1 
+}
+
