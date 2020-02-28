f631201288f3e2abc01a47edaeeaaf3965060ca0
Daniel DeCarlo <44644735+danielcd987@users.noreply.github.com>
2020-02-28 16:05:42 -0500

    Add files via upload

---
+++ To-do-list/process.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+    if(isset($_POST['submit'])){ //checks if button is clicked
+        $task = $_POST['task'];
+        $class = $_POST['class'];
+        $Ddate = $_POST['duedate'];
+        $description = $POST['description'];
+
+    }
+
+
+?>
+
+
+
+</body>
+</html>
