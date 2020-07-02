a0d128de88d0cf91b10ba8555cf3c2210be61859
Daniel DeCarlo <44644735+danielcd987@users.noreply.github.com>
2020-07-01 21:25:08 -0400

    Create header.php

---
+++ My Website/header.php
@@ -0,0 +1,12 @@
+<!DOCTYPE HTML>
+<HTML>
+<HEAD>
+<TITLE>Daniel DeCarlo</TITLE>
+<meta charset="UTF-8" />
+<META name="Daniel DeCarlo" description="Personal Site"/>
+<SCRIPT type = "text/javascript"></SCRIPT>
+</HEAD>
+<BODY>
+
+</BODY>
+</HTML>
